8902f667064f8cd6f13036568898a46132e904fa
Newton Manyisa <manyisanewton26@gmail.com>
2024-12-08 17:29:19 +0300

    phase-1-javascript-functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+// Function declaration: saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function expression: mondayWork
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Function: wrapAdjective
+  function wrapAdjective(wrapper = "*") {
+    return function(adjective) {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
+
+  
